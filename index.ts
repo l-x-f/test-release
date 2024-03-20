c7f96f29284f7ac39aa51220021fd27715fd44ad
xiaofei <lixiaofei0627@gmail.com>
2024-03-20 17:51:34 +0800

    add

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
 console.log('0.0.6-')
+
+type TextAreaProps = {
+  placeholder: string
+  clearable: string
+  minlength: string | number
+  rows: string
+}
+type InputProps = {
+  placeholder: string
+  clearable: string
+  minlength: string | boolean
+  size: string
+}
+
+type Comments = Partial<InputProps | TextAreaProps>
+
+const data: Comments = {
+  placeholder: 'InputProps',
+  clearable: 'InputProps',
+  minlength: 'InputProps',
+  size: 'size',
+  rows: ''
+
+  //   rows: 'InputProps'
+}
+
+console.log(data)
+
+type A = {
+  aaa: string
+}
+type B = {
+  bbb: number
+}
+
+type C = Partial<A & B>
+
+const c: C = {
+  aaa: '',
+  bbb: 0
+}
